3512476de2ebf276cbaf43070528256b8dd8b7fb
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-03-29 11:39:51 +0530

    leetcode problem 1470 solution added

---
+++ arrays/src/easy/ShuffleTheArray.java
@@ -0,0 +1,23 @@
+package easy;
+
+import utils.Functions;
+
+public class ShuffleTheArray {
+	public static void main(String[] args) {
+		int[] nums = {2, 5, 1, 3, 4, 7};
+		int[] result = shuffle(nums, 3);
+
+		System.out.println(Functions.toString(result));
+	}
+
+	static int[] shuffle(int[] nums, int n) {
+		int[] result = new int[2 * n];
+		int j = 0;
+
+		for (int i = 0; i < nums.length; i += 2) {
+			result[i] = nums[j++];
+			result[i + 1] = nums[n++];
+		}
+		return result;
+	}
+}
