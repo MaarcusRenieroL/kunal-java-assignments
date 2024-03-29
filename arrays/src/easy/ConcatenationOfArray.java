d996a97cbb8744394aff8664652baf12fd56b33c
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-03-29 09:11:11 +0530

    leetcode problem 1929 solution added

---
+++ arrays/src/easy/ConcatenationOfArray.java
@@ -0,0 +1,23 @@
+package easy;
+
+import utils.Functions;
+
+public class ConcatenationOfArray {
+  public static void main(String[] args) {
+    int[] nums = { 1, 2, 1 };
+    int[] result = getConcatenation(nums);
+
+    System.out.println(Functions.toString(result));
+  }
+
+  static int[] getConcatenation(int[] nums) {
+    int[] result = new int[nums.length * 2];
+
+    for (int i = 0; i < nums.length; i++) {
+      result[i] = nums[i];
+      result[i + nums.length] = nums[i];
+    }
+
+    return result;
+  }
+}
