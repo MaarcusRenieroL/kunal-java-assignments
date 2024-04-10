c61c25d4eaff76dd56db447d2be664ffd6fc79d6
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-04-10 15:46:51 +0530

    leetcode problem 1678 solution added

---
+++ strings/src/easy/GoalParser.java
@@ -0,0 +1,24 @@
+package easy;
+
+public class GoalParser {
+  public static void main(String[] args) {
+    System.out.println(interpret("G()(al)"));
+  }
+
+  static String interpret(String command) {
+    StringBuilder builder = new StringBuilder();
+    for (int i = 0; i < command.length(); i++) {
+      if (command.charAt(i) == 'G') {
+        builder.append('G');
+      } else if (command.charAt(i) == '(') {
+        if (command.charAt(i + 1) == ')') {
+          builder.append('o');
+        } else {
+          builder.append("al");
+        }
+      }
+    }
+
+    return builder.toString();
+  }
+}
