d78b4946baf1129755a165c6f8c986991bc05c0b
Maarcus Reniero L <maarcusreniero.l@gmail.com>
2024-04-11 06:45:31 +0530

    leetcode problem 709 solution added

---
+++ strings/src/easy/LowerCase.java
@@ -0,0 +1,11 @@
+package easy;
+
+public class LowerCase {
+  public static void main(String[] args) {
+    System.out.println(toLowerCase("Hello"));
+  }
+
+  static String toLowerCase(String s) {
+    return s.toLowerCase();
+  }
+}
